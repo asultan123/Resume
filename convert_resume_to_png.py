17320ade04a01dbd6dceb0700542067f93181703
asultan123 <asultan@aucegypt.edu>
2022-02-15 16:44:31 -0500

    testing hook

--- convert_resume_to_png.py
+++ convert_resume_to_png.py
@@ -1,8 +1,14 @@
 
 # import module
 from pdf2image import convert_from_path
+import os
+import glob
 
 RESUME_FOLDER = './output'
+
+files = glob.glob(RESUME_FOLDER + '/*')
+for f in files:
+    os.remove(f)
 
 # Clear readme
 file = open("README.md","w")
@@ -11,7 +17,7 @@ file.close()
 
 # Store Pdf with convert_from_path function
 images = convert_from_path('main.pdf')
-#  ![Resume](https://github.com/asultan123/Resume/blob/master/output/page0.jpg)
+
 for i in range(len(images)):
     images[i].save(RESUME_FOLDER + '/page'+ str(i) +'.jpg', 'JPEG')
     with open('./README.md', 'a') as md_file:
