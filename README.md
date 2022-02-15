e4a81844f37d307f9009d025e8ff961e1d99cc50
asultan123 <asultan@aucegypt.edu>
2022-02-15 16:33:17 -0500

    testing multiple pages

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+See below
 ![Resume](https://github.com/asultan123/Resume/blob/master/output/page0.jpg)
+![Resume](https://github.com/asultan123/Resume/blob/master/output/page1.jpg)
--- convert_resume_to_png.py
+++ convert_resume_to_png.py
@@ -4,9 +4,15 @@ from pdf2image import convert_from_path
 
 RESUME_FOLDER = './output'
 
+# Clear readme
+file = open("README.md","w")
+file.write('See below\n')
+file.close()
+
 # Store Pdf with convert_from_path function
 images = convert_from_path('main.pdf')
- 
+#  ![Resume](https://github.com/asultan123/Resume/blob/master/output/page0.jpg)
 for i in range(len(images)):
-      # Save pages as images in the pdf
     images[i].save(RESUME_FOLDER + '/page'+ str(i) +'.jpg', 'JPEG')
+    with open('./README.md', 'a') as md_file:
+        md_file.write(f'![Resume](https://github.com/asultan123/Resume/blob/master/output/page{i}.jpg)\n')
